f502fa5dd1cf4d8e83dcb9effa929df40a91cec2
Roche Stephane <sroche@srv-tp04.insa-toulouse.fr>
2022-05-28 17:33:34 +0200

    be fini, algos fonctionnent

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -117,7 +117,6 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
             solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));  
 				
         }
-
         return solution;
     }
     protected Label newLabel(Node node,double cout, ShortestPathData data) {
